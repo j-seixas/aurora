c419a1032157a635771c39c6dcb39608300bacfa
João Seixas <up201505648@fe.up.pt>
2019-04-18 19:54:20 +0100

    Added weapon and attack animation

---
+++ Aurora/Assets/Scripts/Attact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attact : MonoBehaviour
+{
+    float AttackTime;
+    int AttackDamage;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire3")) {
+            print("Fire");
+            this.animator.Play("Attack");
+        }
+    }
+}
