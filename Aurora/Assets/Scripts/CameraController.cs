2c03e01ab999a6141a36e8778f2dd4b8ef45771e
unknown <joaodiasconde@gmail.com>
2019-04-20 21:14:22 +0100

    Camera controller

--- Aurora/Assets/Scripts/CameraController.cs
+++ Aurora/Assets/Scripts/CameraController.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//based on https://www.mvcode.com/lessons/unity-rpg-camera-controller-jamie
+//TODO: delete when delivering
 public class CameraController : MonoBehaviour {
-    private Transform player;
+    public GameObject target;
+
     private Vector3 offset;
 
+    private float pitch = 0f;   //up/down
+    private float minPitch = -40f, maxPitch = 60f;
+    private float yaw = 0f;     //left/right
+    private float roll = 0f;    //roll around
+
+    private float sensitivity = 2f;
+
     void Start() {
-        this.player = GameObject.FindWithTag("Player").transform;
-        this.offset = player.position - transform.position;
+        this.offset = target.transform.position - transform.position;
     }
 
     void Update() {
-        transform.position = player.position - this.offset;
+        Vector3 directedOffset = offset.magnitude * transform.forward;
+        directedOffset.y = offset.y;
+        transform.position = target.transform.position - directedOffset;
+
+        yaw += sensitivity * Input.GetAxis("Mouse X");
+        pitch += sensitivity * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        transform.localEulerAngles = new Vector3(pitch, yaw, roll);
     }
 }
--- Aurora/Assets/Scripts/PlayerController.cs
+++ Aurora/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    
+    public Camera mainCam;
+    
     private Rigidbody rb;
     private float speed = 10.0f;
 
@@ -11,8 +14,14 @@ public class PlayerController : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        this.rb.MovePosition(transform.position + direction.normalized * Time.deltaTime * this.speed);
+        HandlePlayerMovement();
+    }
+
+    void HandlePlayerMovement(){
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        direction = Quaternion.AngleAxis(mainCam.transform.localEulerAngles.y, Vector3.up) * direction; //take into account camera yaw/direction
+        
+        this.rb.MovePosition(transform.position + direction * Time.deltaTime * this.speed);
         
         if (direction != Vector3.zero)
             this.rb.MoveRotation(Quaternion.LookRotation(direction));
