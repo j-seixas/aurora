7a35ed4e11bb1bd5f7699895f656c72f2d97467d
unknown <joaodiasconde@gmail.com>
2019-04-23 19:14:17 +0100

    fixed camera snap when rotating, now a smooth linear interpolation

--- Aurora/Assets/Scripts/CameraController.cs
+++ Aurora/Assets/Scripts/CameraController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
     public GameObject target;
-    public float sensitivity = 2f;
 
-    private Vector3 offset;
-    private float pitch = 0f;   //up/down
-    private float yaw = 0f;     //left/right
+    private Vector3 offset;                         //camera-target distance to be kept
+    private float pitch = 0f;                       //rotation around X axis
+    private float yaw = 0f;                         //rotation around Y axis
     private float minPitch = 0f, maxPitch = 85f;
+    private float sensitivity = 2f;                 //input sensitivity multiplier
+    private float cameraRotationSpeed = 4f;         //how fast camera rotates, the slower the smoother
 
     void Start() {
         this.offset = target.transform.position - transform.position;
@@ -21,6 +22,6 @@ public class CameraController : MonoBehaviour {
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
 
         transform.position = target.transform.position - offset.magnitude * transform.forward;
-        transform.localEulerAngles = new Vector3(pitch, yaw, 0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(pitch, yaw, 0f)), cameraRotationSpeed * Time.deltaTime);
     }
 }
