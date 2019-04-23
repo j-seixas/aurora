0928f0cac1991666f169dd6b3df06b871c354238
joao-conde <up201503256@fe.up.pt>
2019-04-23 17:37:29 +0100

    Camera rotates around player in the vertical axis aswell

--- Aurora/Assets/Scripts/CameraController.cs
+++ Aurora/Assets/Scripts/CameraController.cs
@@ -9,20 +9,18 @@ public class CameraController : MonoBehaviour {
     private Vector3 offset;
     private float pitch = 0f;   //up/down
     private float yaw = 0f;     //left/right
-    private float minPitch = -40f, maxPitch = 60f;
+    private float minPitch = 0f, maxPitch = 85f;
 
     void Start() {
         this.offset = target.transform.position - transform.position;
     }
 
     void Update() {
-        Vector3 directedOffset = offset.magnitude * transform.forward;
-        directedOffset.y = offset.y;
-        transform.position = target.transform.position - directedOffset;
-
         yaw += sensitivity * Input.GetAxis("Mouse X");
         pitch += sensitivity * Input.GetAxis("Mouse Y");
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        transform.position = target.transform.position - offset.magnitude * transform.forward;
         transform.localEulerAngles = new Vector3(pitch, yaw, 0f);
     }
 }
