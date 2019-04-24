7a27c1680d960e8fae267f3dffdc9500590c1090
aquelemiguel <miguelmanofernandes@gmail.com>
2019-04-24 19:18:03 +0100

    Script folder refactor

---
+++ Aurora/Assets/Scripts/Waves/WaveController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+    public DateTime remainingTime;
+    public int minCount, spawnPerRate;
+    public float spawnRateBase, spawnRateMultiplier;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
---
+++ Aurora/Assets/Scripts/Waves/WaveFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFactory : MonoBehaviour {
+    public GameObject waveController;
+    private List<WaveController> waves = new List<WaveController>();
+    
+    void Start() {
+    }
+
+    void Update() {
+        
+    }
+
+}
