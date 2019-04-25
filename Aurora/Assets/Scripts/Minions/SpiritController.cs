8164435ac3efbd223d7f9d650b8daece4797670f
João Seixas <up201505648@fe.up.pt>
2019-04-25 22:58:25 +0100

    Fixing SpiritController that sometimes Player could "escape"

--- Aurora/Assets/Scripts/Minions/SpiritController.cs
+++ Aurora/Assets/Scripts/Minions/SpiritController.cs
@@ -5,24 +5,33 @@ using UnityEngine.UI;
 
 public class SpiritController : MonoBehaviour {
     private Transform player;
+    private bool playerInRange = false;
+    private Text spiritCountLabel;
+
     void Start() {
         this.player = GameObject.FindGameObjectWithTag("Player").transform;
+        spiritCountLabel = GameObject.Find("SpiritCount").GetComponent<Text>();
     }
 
-    void OnTriggerStay(Collider other) {
-        if (other.tag == "Player") {
-            float distance = Vector3.Distance(transform.position, other.transform.position);
-            
+    void Update() {
+        if (playerInRange) {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+
             // Collect spirit.
             if (distance >= 0.0f && distance < 0.50f) {
                 int spiritCount;
-                Text spiritCountLabel = GameObject.Find("SpiritCount").GetComponent<Text>();
+                
                 int.TryParse(spiritCountLabel.text, out spiritCount);
                 spiritCountLabel.text = (spiritCount + 1).ToString();
 
                 Destroy(gameObject);
             }
-            transform.position = Vector3.MoveTowards(transform.position, other.transform.position, Time.deltaTime * 100.0f * 0.6f/distance);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 100.0f * 0.6f / distance);
         }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+            playerInRange = true;
     }
 }
