4a2d887938f973698f871590503ab06549ffec1c
Miguel Fernandes <miguelmanofernandes@gmail.com>
2019-04-26 21:21:31 +0100

    Simple single-type minion spawn at fixed rate

--- Aurora/Assets/Scripts/Waves/WaveController.cs
+++ Aurora/Assets/Scripts/Waves/WaveController.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
 
 public class WaveController : MonoBehaviour {
-    public DateTime remainingTime;
-    public int minCount, spawnPerRate;
-    public float spawnRateBase, spawnRateMultiplier;
+
+    // The minimum no. of minions in the field at all times.
+    public int minCount;
+
+    // The spawn base frequency of minions.
+    public float spawnRateBase;
+
+    // The no. of minions spawned per rate.
+    public int spawnPerRate;
+
+    // A multiplier which affects the base spawn rate. Changes with more time elapsed.
+    public float spawnRateMultiplier;
+    
+    // The wave's remaining time. Always decreases.
+    public float remainingTime;
+
+    // The melee and ranged minion game objects.
+    public GameObject minionMelee, minionRanged;
 
     void Start() {
-        
+        StartCoroutine(SpawnMinion(1, 2f));
     }
+
+    public void Setup(int minCount, float spawnRateBase, int spawnPerRate, float spawnRateMultiplier, float remainingTime) {
+        this.minCount = minCount; 
+        this.spawnRateBase = spawnRateBase;
+        this.spawnPerRate = spawnPerRate;
+        this.spawnRateMultiplier = spawnRateMultiplier;
+        this.remainingTime = remainingTime;
+    }
+
+    private IEnumerator SpawnMinion(int spawnPerRate, float spawnRateBase) {
+        while (true) {
+            yield return new WaitForSeconds(spawnRateBase);
+
+            for (int i = 0; i < spawnPerRate; i++) {
+                Instantiate(this.minionRanged, new Vector3(Random.Range(-20f, 20f), 0.75f, Random.Range(-20f, 20f)), Quaternion.identity);
+            }
+        }
+    }
+
 
     void Update() {
         
--- Aurora/Assets/Scripts/Waves/WaveFactory.cs
+++ Aurora/Assets/Scripts/Waves/WaveFactory.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class WaveFactory : MonoBehaviour {
     public GameObject waveController;
-    private List<WaveController> waves = new List<WaveController>();
+    private List<GameObject> waves = new List<GameObject>();
     
     void Start() {
+        GameObject wave1 = Instantiate(waveController, Vector3.zero, Quaternion.identity);
+        wave1.GetComponent<WaveController>().Setup(10, 5f, 2, 1, 30f);
+        this.waves.Add(wave1);
     }
 
     void Update() {
