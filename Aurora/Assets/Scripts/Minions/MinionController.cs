45928f921566fd1968ea47b797c9a90cfbeaef85
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2019-04-27 17:00:01 +0100

    Added minion add navmesh components scripts

---
+++ Aurora/Assets/Scripts/Minions/MinionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MinionController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,lookRadius);
+    }
+}
