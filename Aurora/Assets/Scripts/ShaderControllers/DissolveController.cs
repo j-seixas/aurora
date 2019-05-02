8844a30ee3d5b1d00a259ceb43413c6e31d79c18
aquelemiguel <miguelmanofernandes@gmail.com>
2019-05-02 19:17:21 +0100

    Change yucky skybox and add fog

---
+++ Aurora/Assets/Scripts/ShaderControllers/DissolveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour {
+    public float animationSpeed = 0.01f;
+    private Material material;
+    private bool canDissolve = false;
+
+    void Start() {
+        this.material = gameObject.GetComponent<Renderer>().material;
+    }
+
+    void Update() {
+        if (this.canDissolve) {
+            this.material.SetFloat("_AnimationFrame", this.material.GetFloat("_AnimationFrame") + this.animationSpeed);
+        }
+        if (this.material.GetFloat("_AnimationFrame") >= 1) {
+            this.canDissolve = false;
+            this.gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void StartDissolving() {
+        this.canDissolve = true;
+    }
+}
