e7e823d5e882ed7d431c43b0f330fcaf0928b622
Sofia Silva <up201505628@fe.up.pt>
2019-05-19 19:58:28 +0100

    the most beautiful menu :pig:

---
+++ Aurora/Assets/Scripts/UI/SelectOnInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectOnInput : MonoBehaviour {
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    void Start () {
+        Text[] children = selectedObject.GetComponentsInChildren<Text> (true);
+
+        for (int i = 0; i < children.Length; i++) {
+            if (children[i].name == "ArrowText")
+                children[i].gameObject.SetActive (true);
+
+        }
+    }
+
+    void Update () {
+        if (Input.GetAxisRaw ("Vertical") != 1) {
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag ("MenuPointer")) {
+                obj.SetActive (false);
+            }
+
+            selectedObject = eventSystem.currentSelectedGameObject;
+            Text[] children = selectedObject.GetComponentsInChildren<Text> (true);
+
+            for (int i = 0; i < children.Length; i++) {
+                if (children[i].name == "ArrowText")
+                    children[i].gameObject.SetActive (true);
+            }
+        }
+
+        if (Input.GetButtonDown ("Submit")) {
+            if (selectedObject.name == "QuitButton")
+                Application.Quit ();
+            else if (selectedObject.name == "StartButton") {
+                SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+            }
+        }
+    }
+}
