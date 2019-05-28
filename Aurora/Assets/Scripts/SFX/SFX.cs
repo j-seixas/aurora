ff29ecf3103158c651768e6ccf97e26152c0361c
Vicente <vicente3109@gmail.com>
2019-05-28 16:39:40 +0100

    added mountain sound

---
+++ Aurora/Assets/Scripts/SFX/SFX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour {
+    public AudioSource earthquake;
+
+    public void playEarthQuake () {
+        earthquake.Play ();
+    }
+
+    public void onTriggerEnter(){
+        earthquake.Play();
+    }
+}
