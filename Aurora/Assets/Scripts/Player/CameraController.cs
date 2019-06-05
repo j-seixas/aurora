936a586a76aa518eff69da65037192b5f096ef25
joao-conde <up201503256@fe.up.pt>
2019-06-05 01:04:42 +0100

    CameraController fix

--- Aurora/Assets/Scripts/Player/CameraController.cs
+++ Aurora/Assets/Scripts/Player/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour {
     private Vector3 offset;                         //camera-target distance to be kept
     private float pitch = 0f;                       //rotation around X axis
     private float yaw = 0f;                         //rotation around Y axis
-    private float minPitch = -27f, maxPitch = 85f;
+    private float minPitch = -5f, maxPitch = 85f;
     private float sensitivity = 2f;                 //input sensitivity multiplier
     private float cameraRotationSpeed = 4f;         //how fast camera rotates, the slower the smoother
 
@@ -21,15 +21,15 @@ public class CameraController : MonoBehaviour {
         yaw += sensitivity * Input.GetAxis("Mouse X");
         pitch += sensitivity * Input.GetAxis("Mouse Y");
     
-        if(Input.GetButtonDown("Recenter Camera")) Recenter();
+        // if(Input.GetButtonDown("Recenter Camera")) Recenter();
         
         UpdateCameraTransform();
     }
 
     void UpdateCameraTransform(){
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch); 
-        transform.position = target.transform.position - offset.magnitude * transform.forward - Vector3.up * offset.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(pitch, yaw, 0f)), cameraRotationSpeed * Time.deltaTime);
+        transform.position = target.transform.position - offset.magnitude * transform.forward - Vector3.up * offset.y;
         //TODO: Slerp vs Lerp, try it at will, probably add to settings?
     }
 
