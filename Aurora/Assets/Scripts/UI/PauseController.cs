502132324d798f597f01f57d5f4b4efad1e4c21c
Sofia Silva <up201505628@fe.up.pt>
2019-06-09 03:01:24 +0100

    added pause menu

---
+++ Aurora/Assets/Scripts/UI/PauseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+
+    public GameObject pausePanel;
+
+    void Update () {
+        if (Input.GetButtonDown ("Cancel")) {
+            TogglePause ();
+        }
+    }
+
+    public void TogglePause () {
+        if (Time.timeScale == 0f) {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+        } else {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+        }
+    }
+}
