550febdf1a4706364c579adc3c8d7acba8249f4b
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2019-06-09 04:31:09 +0100

    Add skybox change on runtime and pre calculated cubemaps for lighting

---
+++ Aurora/Assets/Scripts/ShopController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public Material blackSkybox;
+    public Cubemap blackCubemap;
+
+
+    private Material normalSkybox;
+
+    private Cubemap normalCubemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalSkybox = RenderSettings.skybox;
+        normalCubemap = RenderSettings.customReflection;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("HELLO enter");
+        if(other.tag == "PlayerBody"){
+            RenderSettings.skybox = blackSkybox;
+            RenderSettings.customReflection = blackCubemap;
+            DynamicGI.UpdateEnvironment();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "PlayerBody"){
+            RenderSettings.skybox = normalSkybox;
+            RenderSettings.customReflection = normalCubemap;
+            DynamicGI.UpdateEnvironment();
+        }
+    }
+}
