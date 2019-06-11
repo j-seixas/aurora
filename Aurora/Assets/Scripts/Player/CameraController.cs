f472cabfb11cf0d71cbd6325c4c1a752ded1ecd7
joao-conde <up201503256@fe.up.pt>
2019-06-11 17:34:19 +0100

    FINAL TERRAIN - I WILL NOT EDIT IT MORE, CHANGE AT WILL AND OWN RISK

--- Aurora/Assets/Scripts/Player/CameraController.cs
+++ Aurora/Assets/Scripts/Player/CameraController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public float sensitivity;               //input sensitivity multiplier
+    
     private GameObject target;
-
     private Vector3 offset;                         //camera-target distance to be kept
     private float pitch = 0f;                       //rotation around X axis
     private float yaw = 0f;                         //rotation around Y axis
     private float minPitch = -5f, maxPitch = 85f;
-    private float sensitivity = 2f;                 //input sensitivity multiplier
     private float cameraRotationSpeed = 4f;         //how fast camera rotates, the slower the smoother
 
     void Start() {
