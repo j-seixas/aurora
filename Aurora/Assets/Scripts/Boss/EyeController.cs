149e2a2e3f1c597199078472dba8303ccd811d67
unknown <joaodiasconde@gmail.com>
2019-06-19 17:12:25 +0100

    Eye directly looks at Aurora, rotation

--- Aurora/Assets/Scripts/Boss/EyeController.cs
+++ Aurora/Assets/Scripts/Boss/EyeController.cs
@@ -1,11 +1,12 @@
-using System.Collections;
 using UnityEngine;
 
 public class EyeController : MonoBehaviour {
     public Transform target;
 
+    public Vector3 rotationOffset;
+
     void Update () {
         transform.LookAt (target);
-
+        transform.Rotate(rotationOffset);
     }
 }
